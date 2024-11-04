31b4d21bdab6ba63126ed1a884881af97e529b14
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-04 16:59:33 +0300

    Create ResponseData.java

---
+++ src/main/java/data/ResponseData.java
@@ -0,0 +1,26 @@
+package data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseData {
+    private int x;
+    private double y;
+    private double r;
+    private boolean isIn;
+    private long executionTime;
+    private String serverTime;
+
+    public ResponseData (int x, double y, double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.isIn = false;
+    }
+}
