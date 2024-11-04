a71a005100614694f1dafc5c089e9502995708f9
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-04 17:00:47 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import com.fastcgi.FCGIInterface;
+import handlers.RequestHandler;
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println("hello wrld");
-
+        var fcgiInterface = new FCGIInterface();
+        var requestHandler = new RequestHandler();
+        while (fcgiInterface.FCGIaccept() >= 0) {
+            requestHandler.handleRequest(FCGIInterface.request);
+        }
     }
 }
