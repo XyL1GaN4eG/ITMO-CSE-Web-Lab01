622fe5bfc2e3a4eaf94097388588fae89a595a00
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-04 16:20:14 +0300

    rename GeometryValidator to AreaCheck

---
+++ src/main/java/consts/Consts.java
@@ -0,0 +1,24 @@
+package consts;
+
+public class Consts {
+
+    public static final String HTTP_RESPONSE = """
+        HTTP/1.1 200 OK
+        Content-Type: application/json
+        Content-Length: %d
+        
+        %s
+        """;
+    public static final String HTTP_ERROR = """
+        HTTP/1.1 400 Bad Request
+        Content-Type: application/json
+        Content-Length: %d
+        
+        %s
+        """;
+    public static final String ERROR_JSON = """
+        {
+            "reason": "%s"
+        }
+        """;
+}
--- src/main/java/handlers/GeometryValidator.java
+++
@@ -1,7 +0,0 @@
-package handlers;
-
-public class GeometryValidator {
-    public boolean validate(int x, float y, float r) {
-        return true;
-    }
-}
