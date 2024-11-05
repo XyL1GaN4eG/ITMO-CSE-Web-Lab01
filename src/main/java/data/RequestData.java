8f9c1ac11124b4bb1df64758cd015045eb645ab4
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-05 16:34:47 +0300

    Add override toString() to RequestData

--- src/main/java/data/RequestData.java
+++ src/main/java/data/RequestData.java
@@ -1,4 +1,15 @@
 package data;
 
 public record RequestData(int[] x, double y, double r) {
+    @Override
+    public String toString() {
+        var res = "x = ";
+        if (x != null) {
+            for (int element : x) {
+                res += element + ", ";
+            }
+        }
+        res += "y = " + y + ", r = " + r;
+        return res;
+    }
 }
