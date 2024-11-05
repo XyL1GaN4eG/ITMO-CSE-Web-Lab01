6be0eb02c55bc535cebfcd781db0c9d7b078d438
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-05 21:55:15 +0300

    Some refactor in RequestHandler

--- src/main/java/handlers/RequestHandler.java
+++ src/main/java/handlers/RequestHandler.java
@@ -5,6 +5,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import data.RequestData;
 import data.ResponseData;
+import exceptions.InvalidRequestException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 import static consts.Consts.*;
 
@@ -21,59 +23,58 @@ public class RequestHandler {
     private final AreaCheck areaCheck = new AreaCheck();
     private final DateTimeFormatter yyyymmddhhmmss = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private FCGIRequest request;
-    Gson gson = new GsonBuilder()
+    private final Gson gson = new GsonBuilder()
             .registerTypeAdapter(RequestData.class, new RequestDataAdapter()) //используем кастомный
             .setPrettyPrinting()
             .create();
 
 
     public void handleRequest(FCGIRequest request) {
-        log.info("получен следующий реквест: {}", request);
         var params = request.params;
-        //todo: нормальная ошибка
-
         var requestMethod = params.getProperty("REQUEST_METHOD");
         log.info("Получен реквест со следующим методом: {}", requestMethod);
         this.request = request;
-        switch (requestMethod) {
+        try {
+            chooseHTTPMethod(requestMethod);
+        } catch (InvalidRequestException e) {
+            sendError(e.getMessage());
+        }
+    }
+
+    //todo: добавить поддержку истории
+    private void chooseHTTPMethod(String requestMethod) throws InvalidRequestException {
+        switch (requestMethod.toUpperCase()) {
             case "POST" -> handlePOST();
             case "GET" -> handleGET();
-            default -> sendError("Пришел неизвестный метод");
+            case "DELETE" -> handleDELETE();
+            default -> throw new InvalidRequestException("Invalid HTTP method");
         }
     }
 
     //todo: дописать метод
-    private void handleGET() {
-        log.info("Начинаем обрабатывать get запрос");
+    private void handleDELETE() {
+        log.info("Начинаем обрабатывать DELETE запрос");
     }
 
-    private void handlePOST() {
-        log.info("Начинаем обрбатывать post запрос");
-        log.info("Попытка getRequestData()");
-        var requestsArray = getRequestData();
-        try {
-            log.info("Распарсили реквест: {}", requestsArray.toString());
-        } catch (NullPointerException e) {
-            log.error("Ошибка при парсинге requestsArray: ", e);
-        }
+    //todo: дописать метод
+    private void handleGET() {
+        log.info("Начинаем обрабатывать GET запрос");
+    }
+
+    private void handlePOST() throws InvalidRequestException {
+        log.info("Начинаем обрабатывать POST запрос");
+        RequestData requestsArray = getRequestData();
+        log.info("Распарсили реквест: {}", requestsArray);
 
         var responseArr = getResponseData(requestsArray);
         var response = responseArrayToJSON(responseArr);
 
-        log.info("Провалидировали данные: \n{}", response);
+        log.info("Готовый для отправки ответ: \n{}", response);
 
-        try {
-            log.info("Попытка записать полученные данные в request.outStream");
-            request.outStream.write(response.getBytes(StandardCharsets.UTF_8));
-            log.info("Попытка смыть данные: request.outStream.flush()");
-            request.outStream.flush();
-
-        } catch (IOException e) {
-            log.error("Произошла ошибка при записи/смыве данных: {}", e.getMessage());
-        }
+        sendResponse(response);
     }
 
-    private String responseArrayToJSON(ArrayList<ResponseData> responseArr) {
+    private String responseArrayToJSON(List<ResponseData> responseArr) {
         var json = gson.toJson(responseArr);
         return String.format(
                 HTTP_RESPONSE,
@@ -81,59 +82,72 @@ public class RequestHandler {
         );
     }
 
-    private ArrayList<ResponseData> getResponseData(RequestData requestsArray) {
+    private List<ResponseData> getResponseData(RequestData requestsArray) {
         var response = new ResponseData();
         var responseArr = new ArrayList<ResponseData>();
         for (int x : requestsArray.x()) {
-            long startTime = System.nanoTime();
-            response = new ResponseData(x,
-                    requestsArray.y(),
-                    requestsArray.r());
-            response.setIn(areaCheck.validate(response));
-            response.setServerTime(LocalDateTime.now().format(yyyymmddhhmmss));
-            response.setExecutionTime(System.nanoTime() - startTime);
+            response = createElementOfResponse(requestsArray, x);
             responseArr.add(response);
         }
         return responseArr;
     }
 
-    private void sendError(String msg) {
-        try {
-            //todo: нормальная ошибка пожалуйста
-            String json = String.format(ERROR_JSON, "пук пук ниче не выполнилось т.к. " + msg).trim();
-            String response = String.format(HTTP_ERROR, json.getBytes(StandardCharsets.UTF_8).length, json);
-            System.out.println(response);
-            request.outStream.write(response.getBytes(StandardCharsets.UTF_8));
-            request.outStream.flush();
-        } catch (IOException ex) {
-            log.error("Ошибка при отправке исключения {} на клиент: ", msg, ex);
-        }
+    private ResponseData createElementOfResponse(RequestData requestsArray, int x) {
+        ResponseData response;
+        long startTime = System.nanoTime();
+        response = new ResponseData(x,
+                requestsArray.y(),
+                requestsArray.r());
+        response.setIn(areaCheck.validate(response));
+        response.setServerTime(LocalDateTime.now().format(yyyymmddhhmmss));
+        response.setExecutionTime(System.nanoTime() - startTime);
+        return response;
     }
 
-    private RequestData getRequestData() {
-        try {
-            request.inStream.fill();
-            var contentLength = request.inStream.available();
-            var buffer = ByteBuffer.allocate(contentLength);
-            var readBytes = request.inStream.read(
-                    buffer.array(),
-                    0,
-                    contentLength);
-            var requestBodyRaw = new byte[readBytes];
-            buffer.get(requestBodyRaw);
-            buffer.clear();
-            String requestString = new String(requestBodyRaw, StandardCharsets.UTF_8);
-            log.info("Получили следующую строку при попытке парсинга: {}", requestString);
-            RequestData data = gson.fromJson(requestString, RequestData.class);
-            return data;
-        } catch (IOException e) {
-            log.error("Ошибка при парсинге RequestData: ", e);
 
-            //создаем реквестдату которая точно не попадет в промежуток в случае ошибки парсинга
-            return new RequestData(
-                    new int[]{-20},
-                    -20,
-                    -20);
+    private RequestData getRequestData() throws InvalidRequestException {
+        try {
+            String requestString;
+            requestString = readJSON();
+            log.info("Получили следующий JSON при попытке чтения из буфера: {}", requestString);
+            RequestData data = gson.fromJson(requestString, RequestData.class);
+            log.info("Строка успешно привелась к объекту: {}", data.toString());
+            return data;
+        } catch (NullPointerException | IOException e) {
+            throw new InvalidRequestException("Invalid JSON");
+        }
+
+    }
+
+    private String readJSON() throws IOException {
+        request.inStream.fill();
+        var contentLength = request.inStream.available();
+        var buffer = ByteBuffer.allocate(contentLength);
+        var readBytes = request.inStream.read(
+                buffer.array(),
+                0,
+                contentLength);
+        var requestBodyRaw = new byte[readBytes];
+        buffer.get(requestBodyRaw);
+        buffer.clear();
+        return new String(requestBodyRaw, StandardCharsets.UTF_8);
+    }
+
+    private void sendError(String msg) {
+        String json = String.format(ERROR_JSON, msg).trim();
+        String response = String.format(HTTP_ERROR, json.getBytes(StandardCharsets.UTF_8).length, json);
+        sendResponse(response);
+    }
+
+    private void sendResponse(String response) {
+        try {
+            log.info("Попытка записать полученные данные в request.outStream");
+            request.outStream.write(response.getBytes(StandardCharsets.UTF_8));
+            log.info("Попытка смыть данные: request.outStream.flush()");
+            request.outStream.flush();
+            log.info("Данные отправлены без ошибок!");
+        } catch (IOException e) {
+            log.error("Произошла ошибка при отправлении данных на клиент: ", e);
         }
     }
 }
