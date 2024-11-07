fd7fde78a79edb4876c8e8fbe16c06c4af7bdae5
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-07 19:45:21 +0300

    Refactor consts.java

--- src/main/java/consts/Consts.java
+++ src/main/java/consts/Consts.java
@@ -1,24 +1,23 @@
 package consts;
 
 public class Consts {
-
     public static final String HTTP_RESPONSE = """
-        HTTP/1.1 200 OK
-        Content-Type: application/json
-        Content-Length: %d
-        
-        %s
-        """;
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+            Content-Length: %d
+
+            %s
+            """;
     public static final String HTTP_ERROR = """
-        HTTP/1.1 400 Bad Request
-        Content-Type: application/json
-        Content-Length: %d
-        
-        %s
-        """;
+            HTTP/1.1 400 Bad Request
+            Content-Type: application/json
+            Content-Length: %d
+                        
+            %s
+            """;
     public static final String ERROR_JSON = """
-        {
-            "reason": "%s"
-        }
-        """;
+            {
+                "reason": "%s"
+            }
+            """;
 }
