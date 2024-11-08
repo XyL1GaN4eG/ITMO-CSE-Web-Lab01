ef4055bc7f37a2ec14d5c793dc5bf5fdf57d9023
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-08 19:55:30 +0300

    Create ResponseSender

---
+++ src/main/java/handlers/ResponseSender.java
@@ -0,0 +1,43 @@
+package handlers;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+@NoArgsConstructor
+@Slf4j
+public class ResponseSender {
+    public void sendResponse(OutputStream outStream, String jsonBody) {
+        try {
+            var headers = new Properties();
+            headers.put("Content-Type", "application/json");
+            headers.put("Content-Length", String.valueOf(jsonBody.getBytes(StandardCharsets.UTF_8).length));
+            sendBuiltResponse(outStream, headers, jsonBody);
+        } catch (IOException e) {
+            log.error("Ошибка при отправке ответа: ", e);
+        }
+    }
+
+    public void sendError(OutputStream outStream, String message) throws IOException {
+        Properties headers = new Properties();
+        headers.put("Content-Type", "text/html"); // Устанавливаем Content-Type на text/html
+        headers.put("Content-Length", String.valueOf(message.getBytes(StandardCharsets.UTF_8).length));
+
+        sendBuiltResponse(outStream, headers, message);
+    }
+
+    private void sendBuiltResponse(OutputStream outStream, Properties headers, String message) throws IOException {
+        StringBuilder headerBuilder = new StringBuilder();
+        for (String key : headers.stringPropertyNames()) {
+            headerBuilder.append(key).append(": ").append(headers.getProperty(key)).append("\r\n");
+        }
+        headerBuilder.append("\r\n"); // Завершаем заголовки пустой строкой
+        outStream.write(headerBuilder.toString().getBytes());
+        outStream.write(message.getBytes(StandardCharsets.UTF_8));
+        outStream.flush();
+    }
+}
--- src/main/java/handlers/commands/RequestHandler.java
+++ src/main/java/handlers/commands/RequestHandler.java
@@ -3,84 +3,35 @@ package handlers.commands;
 import com.fastcgi.FCGIRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import data.ResponseData;
 import exceptions.InvalidRequestException;
+import handlers.ResponseSender;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-import static consts.Consts.*;
 
 @Slf4j
 public class RequestHandler {
-    private FCGIRequest request;
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final ResponseSender responseSender = new ResponseSender();
 
-    public void handleRequest(FCGIRequest request) {
-        var params = request.params;
-        var requestMethod = params.getProperty("REQUEST_METHOD");
-        this.request = request;
-        log.info("Получен реквест со следующим методом: {}", requestMethod);
+    public void handleRequest(FCGIRequest request) throws IOException {
         try {
-            HttpCommand command = chooseHTTPMethod(requestMethod, request);
-            var response = command.execute();
-            if (response != null) {
-//                String json = responseArrayToJSON(response);
-                String json = stringToResponse();
-                log.info("Получили непустой ответ: {}, начинаем отправлять", json);
-                sendResponse(json);
-            } else {
-                log.info("Обработан запрос, не нуждающийся в ответе");
-            }
+            // used command pattern for processing requests
+            var command = chooseHTTPMethod(request);
+            var result = command.execute();
+            var json = gson.toJson(result);
+            responseSender.sendResponse(request.outStream, json);
         } catch (InvalidRequestException e) {
-            sendError(e.getMessage());
+            responseSender.sendError(request.outStream, e.getMessage());
         }
     }
 
-    private HttpCommand chooseHTTPMethod(String requestMethod, FCGIRequest request) throws InvalidRequestException {
-        return switch (requestMethod.toUpperCase()) {
+    private HttpCommand chooseHTTPMethod(FCGIRequest request) throws InvalidRequestException {
+        return switch (request.params.getProperty("REQUEST_METHOD").toUpperCase()) {
             case "POST" -> new Post(request);
             case "GET" -> new Get();
             case "DELETE" -> new Delete();
             default -> throw new InvalidRequestException("Invalid HTTP method");
         };
-    }
-
-    private void sendError(String msg) {
-        String json = String.format(ERROR_JSON, msg).trim();
-        String response = String.format(HTTP_ERROR, json.getBytes(StandardCharsets.UTF_8).length, json);
-        sendResponse(response);
-    }
-
-    private void sendResponse(String response) {
-        System.out.println(response);
-//        try {
-//            log.info("Попытка записать полученные данные в request.outStream");
-//            request.outStream.write(response.getBytes(StandardCharsets.UTF_8));
-//            log.info("Попытка смыть данные: request.outStream.flush()");
-//            request.outStream.flush();
-//            log.info("Данные отправлены без ошибок!");
-//        } catch (IOException e) {
-//            log.error("Произошла ошибка при отправлении данных на клиент: ", e);
-//        }
-    }
-
-    private String responseArrayToJSON(List<ResponseData> responseArr) {
-        var json = gson.toJson(responseArr);
-        return String.format(
-                HTTP_RESPONSE,
-                json.getBytes(StandardCharsets.UTF_8).length, json
-        );
-    }
-
-    private String stringToResponse() {
-//        var json = gson.toJson();
-        var json = "{\"x\": 1}";
-        return String.format(
-                HTTP_RESPONSE,
-                json.getBytes(StandardCharsets.UTF_8).length, json
-        );
     }
 }
